dc4f67371ef77d9484b533fc0fb668681fc35e86
Raivis Filimonovs <Raivizz@users.noreply.github.com>
2019-04-02 16:54:35 +0300

    1.2.3 update fixes

--- Code/Main/CritHandler.cs
+++ Code/Main/CritHandler.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.IO;
 using System.Collections.Generic;
-using System.Runtime.InteropServices;
 using Terraria;
 using Terraria.DataStructures;
 using Terraria.ModLoader;
--- Code/Main/CustomCritSoundInitializer.cs
+++ Code/Main/CustomCritSoundInitializer.cs
@@ -301,4 +301,3 @@ namespace CritSounds
 			}
         }
     }
-}

90284b76792561eea3c82b29ad017f06db03945d
Raivis <Raivizz@users.noreply.github.com>
2023-01-09 18:17:58 +0200

    Slightly rewrote script

--- lib_external/linux_fetch.sh
+++ lib_external/linux_fetch.sh
@@ -1,50 +1,11 @@
 #!/bin/bash
 
-tempDir="/tmp/bass24-temp"
-cpuArchitecture=$(uname -m)
-
-zipUrls=(
-  "https://www.un4seen.com/files/bass24-linux.zip"
-)
-zipDirectory="."
-
-if [[ $cpuArchitecture == arm* ]]
-then
-  zipUrls=(
-    "http://www.un4seen.com/stuff/bass24-linux-arm.zip"
-  )
-  zipDirectory="hardfp"
-fi
-
-# Create a temp dir
+tempDir="/tmp/libbass-temp"
 mkdir $tempDir
-
-# Go to temp
 cd $tempDir || exit
-
-# Download zip files and extract
-for zipUrl in "${zipUrls[@]}"
-do
-  echo "Downloading $zipUrl file ..."
-
-  wget -qO- -O tmp.zip "$zipUrl" && unzip tmp.zip && rm tmp.zip
-done
-
-# Go to zip directory
-cd $zipDirectory || exit
-
-# Install .so files
-for file in $(find . -maxdepth 1 -name "*.so"); do
-  echo "Installing $file file ..."
-
-  sudo cp "$file" /usr/local/lib
-
-  echo "/usr/local/lib/$file"
-
-  sudo chmod a+rx /usr/local/lib/"$file"
-
-  sudo ldconfig
-done
-
-# Remove tempDir
+echo "Downloading libbass.so"
+wget -qO- -O libbass.so https://github.com/Raivizz/CritSounds/raw/1.4_port/lib_external/libbass.so
+sudo cp libbass.so /usr/local/lib
+sudo chmod a+rx /usr/local/lib/libbass.so
+sudo ldconfig
 rm -r $tempDir
